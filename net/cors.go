d6bae7a82051256bc8be99ab3ec4a7bfd07d476e
Mai Văn Thịnh <thinh@wee.vn>
2026-01-19 15:12:20 +0700

    Refactor CORS middleware to streamline header handling and improve preflight request management

--- net/cors.go
+++ net/cors.go
@@ -12,28 +12,22 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+var (
+	// CORS enabled by default
+	enableCORS = true
+)
+
+func writeHeadersCORS(w http.ResponseWriter) {
+	// Set CORS headers
+	w.Header().Set(corsAllowOriginHeader, "*")
+	w.Header().Set(corsAllowMethodsHeader, "GET, POST, OPTIONS")
+	w.Header().Set(corsAllowHeadersHeader, "*")
+	w.Header().Set(corsMaxAgeHeader, "3600")
+	w.Header().Set(corsAllowCredentialsHeader, "true")
+}
+
 func Middleware(ro *mux.Router, enableCORS bool, middlewareFunc ...http.HandlerFunc) http.Handler {
-	// Define CORS options
-	corsHandler := func(h http.Handler) http.Handler {
-		if !enableCORS {
-			return h
-		}
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Set CORS headers
-			w.Header().Set(corsAllowOriginHeader, "*")
-			w.Header().Set(corsAllowMethodsHeader, "GET, POST, OPTIONS")
-			w.Header().Set(corsAllowHeadersHeader, "*")
-			w.Header().Set(corsMaxAgeHeader, "3600")
-			w.Header().Set(corsAllowCredentialsHeader, "true")
-			// Handle preflight OPTIONS request
-			if r.Method == http.MethodOptions {
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-			// Call the next handler
-			h.ServeHTTP(w, r)
-		})
-	}
+
 	// Apply the middleware functions
 	middlewareHandler := func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -47,35 +41,24 @@ func Middleware(ro *mux.Router, enableCORS bool, middlewareFunc ...http.HandlerF
 			h.ServeHTTP(w, r)
 		})
 	}
-	// Logger interceptor
-	loggerIntecepter := func(h http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Create logger with request context
-			reqLogger := getLogEntry().With(
-				zap.String(logger.KeyNetHttpMethod, r.Method),
-				zap.String(logger.KeyNetHttpPath, r.URL.Path),
-				zap.String(logger.KeyNetHttpQuery, r.URL.RawQuery),
-				zap.String(logger.KeyNetRequestID, r.Header.Get(xApiRequestId)),
-				zap.Object(logger.KeyNetRequestHeaders,
-					zapcore.ObjectMarshalerFunc(func(enc zapcore.ObjectEncoder) error {
-						for k, v := range r.Header {
-							enc.AddString(k, strings.Join(v, "; "))
-						}
-						return nil
-					})),
-			)
-			// Use the context with the logger
-			rc := r.WithContext(setLoggerToContext(r.Context(), reqLogger))
 
-			// Call the next handler with the new context
-			h.ServeHTTP(w, rc)
-		})
+	// Set CORS global flag
+	if enableCORS {
+		fmt.Printf("[DEBUG] CORS is enabled\n")
+		enableCORS = true
+	} else {
+		fmt.Printf("[DEBUG] CORS is disabled\n")
+		enableCORS = false
 	}
-	//
-	ro.Use(corsHandler, middlewareHandler, loggerIntecepter, apiLoggerHandler)
+
+	// Replace the default MethodNotAllowedHandler
+	ro.MethodNotAllowedHandler = customizeMethodNotAllowedHandler()
+
+	// Apply the middleware in order
+	ro.Use(corsHandler, middlewareHandler, loggerIntercepter, apiLoggerHandler)
 
 	// Walk through all the registered routes
-	err := ro.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+	if err := ro.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 		pathTemplate, err := route.GetPathTemplate()
 		if err != nil {
 			return err
@@ -98,10 +81,72 @@ func Middleware(ro *mux.Router, enableCORS bool, middlewareFunc ...http.HandlerF
 			)
 		}
 		return nil
-	})
-	if err != nil {
+	}); err != nil {
 		fmt.Println("Error walking routes: ", err)
 	}
 
 	return ro
+}
+
+// Define CORS options
+func corsHandler(h http.Handler) http.Handler {
+	if !enableCORS {
+		return h
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Handle preflight OPTIONS request
+		if r.Method == http.MethodOptions {
+			// Set CORS headers
+			writeHeadersCORS(w)
+			// Respond with 204 No Content
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		// Set CORS headers
+		writeHeadersCORS(w)
+		// Call the next handler
+		h.ServeHTTP(w, r)
+	})
+}
+
+func loggerIntercepter(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create logger with request context
+		reqLogger := getLogEntry().With(
+			zap.String(logger.KeyNetHttpMethod, r.Method),
+			zap.String(logger.KeyNetHttpPath, r.URL.Path),
+			zap.String(logger.KeyNetHttpQuery, r.URL.RawQuery),
+			zap.String(logger.KeyNetRequestID, r.Header.Get(xApiRequestId)),
+			zap.Object(logger.KeyNetRequestHeaders,
+				zapcore.ObjectMarshalerFunc(func(enc zapcore.ObjectEncoder) error {
+					for k, v := range r.Header {
+						enc.AddString(k, strings.Join(v, "; "))
+					}
+					return nil
+				})),
+		)
+		// Use the context with the logger
+		rc := r.WithContext(setLoggerToContext(r.Context(), reqLogger))
+
+		// Call the next handler with the new context
+		h.ServeHTTP(w, rc)
+	})
+}
+
+func customizeMethodNotAllowedHandler() http.Handler {
+	//
+	fmt.Printf("[DEBUG] Customize MethodNotAllowedHandler default to allow method OPTIONS\n")
+	// Return 403 for Method Not Allowed
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//  for OPTIONS, we have handled it in CORS middleware
+		if r.Method == http.MethodOptions {
+			// Set CORS headers
+			writeHeadersCORS(w)
+			// Respond with 204 No Content
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		fmt.Printf("[DEBUG] 403 Not Found: %s %s\n", r.Method, r.URL.Path)
+		http.Error(w, "403 method not allowed", http.StatusMethodNotAllowed)
+	})
 }
